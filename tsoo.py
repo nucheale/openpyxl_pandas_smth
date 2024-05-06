64c75735c3b75e0b1b1099c40ab4767417c3cbb7
nucheale <stkill488@gmail.com>
2024-05-06 14:12:40 +0300

    wwwwww

--- tsoo.py
+++ tsoo.py
@@ -31,25 +31,32 @@ for wb in files:
 print('1')
 
 rows_to_delete = []
-words_for_delete = ['субботник', 'под загрузку', 'под погрузку', 'установка']
-for row in new_sh.iter_rows(min_row=2):
+schedules_for_delete = ['субботник', 'под загрузку', 'под погрузку', 'установка']
+for row in new_sh.iter_rows(min_row=0):
     if '789' in (str(row[2].value.lower()))[:3]:
         rows_to_delete.append(row[0].row)
     if 'сигнальный' in row[3].value.lower() or 'вкп' in row[3].value.lower():
         rows_to_delete.append(row[0].row)
-    if 'помешочный сбор' in row[6].value.lower():
+    if 'помешочный сбор' in str(row[6].value).lower():
         rows_to_delete.append(row[0].row)
-    if any(word in row[14].value.lower() for word in words_for_delete):
+    if any(schedule in str(row[14].value).lower() for schedule in schedules_for_delete):
         rows_to_delete.append(row[0].row)
     if 'контейнер выходного дня' in str(row[22].value).lower():
         rows_to_delete.append(row[0].row)
 print('2')
-print(f'len: {len(rows_to_delete)}')
-sys.exit(0)
+rows_to_delete = list(set(rows_to_delete))
+print(f'2 \n len: {len(rows_to_delete)}')
 
-for i in reversed(rows_to_delete):
-    new_sh.delete_rows(i)
+new_sh_2 = new_wb.create_sheet('Итог', 2)
+for row in new_sh.iter_rows(min_row=0):
+    if row[0].row not in rows_to_delete:
+        row_with_values = [cell.value for cell in row]
+        new_sh_2.append(row_with_values)
 print('3')
 
+new_wb.remove(new_sh)
+
 new_wb.save(f'{files_dir}Итог/Реестр КП {datetime.now().strftime("%d.%m.%Y %H_%M")}.xlsx')
+new_wb.close()
+
 print('4')
